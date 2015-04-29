062a10eb9657e529ddeda2aef89947593e38d577
Anders Riutta <git@andersriutta.com>
2015-04-28 17:00:59 -0700

    Use remote repos, not local

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -98,15 +98,15 @@ gulp.task('browserify', ['browserifyPolyfills'], function() {
         // a watch is not set.
         // They are too slow to enable
         // during development.
-        .pipe(buffer())
+        .through(buffer())
         // TODO keep an eye on this issue:
         // https://github.com/floridoo/gulp-sourcemaps/issues/73
         // It's the reason we're using v1.1.0 of gulp-sourcemaps,
         // not the latest version.
-        .pipe(sourcemaps.init({loadMaps: true}))
+        .through(sourcemaps.init({loadMaps: true}))
         // Add transformation tasks to the pipeline here.
-        .pipe(uglify())
-        .pipe(sourcemaps.write('./'))
+        .through(uglify())
+        .through(sourcemaps.write('./'))
     }))
     // Specify the output destination
     .pipe(gulp.dest('./dist/'))
