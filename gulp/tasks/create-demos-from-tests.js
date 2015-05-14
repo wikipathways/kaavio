91f09fedbdbce3a842b1c41f177344c7e20d99b5
Anders Riutta <git@andersriutta.com>
2015-05-14 13:39:06 -0700

    Update build process. Work on getting notifications working again.

---
+++ gulp/tasks/create-demos-from-tests.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+var gulp = require('gulp');
+var highland = require('highland');
+var replace = require('gulp-regex-replace');
+
+gulp.task('create-demos-from-tests', function(done) {
+  // Reading it here (instead of using require) because
+  // the version may have been bumped since require ran.
+  var packageJson = JSON.parse(fs.readFileSync(
+      'package.json', 'utf8'));
+
+  return gulp.src(['./test/web-component-demos/**',
+                   './test/jquery-demos/**',
+                   './test/other-demos/**'],
+                   {base: './test'})
+
+    .pipe(replace({
+      regex: packageJson.name + '-polyfills-dev.bundle',
+      replace: packageJson.name + '-polyfills-' + packageJson.version + '.bundle.min'
+    }))
+    .pipe(replace({
+      regex: packageJson.name + '-dev.bundle',
+      replace: packageJson.name + '-' + packageJson.version + '.bundle.min'
+    }))
+		.pipe(gulp.dest('./demo'));
+});
