c7bcd0aabab708a72a8dc68996fc53e3839febd7
Anders Riutta <git@andersriutta.com>
2015-06-04 15:20:06 -0700

    Work on getting kaavio to use workflow-bob. Clean out unused functions from utils.js.

--- gulp/tasks/watch.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('watch', ['set-watch', 'browser-sync'], function() {
-	// Note: The browserify task handles js recompiling with watchify
-  //gulp.watch('./dist/**', ['update-dev-bundle']);
-	//gulp.watch('./lib/**', ['testDev']);
-});
