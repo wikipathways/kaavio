f8aeb810c92b5dff2570f051510ebe9c59ac09be
Anders Riutta <git@andersriutta.com>
2018-02-17 11:52:54 -0800

    Removed unused files

--- gulp/tasks/copy.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('copy', function(done) {
-  //return done();
-  //*
-  return gulp.src('./dist/**')
-		.pipe(gulp.dest('./demo/lib/kaavio'));
-  //*/
-});
--- gulp/tasks/githubPages.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp');
-var buildBranch = require('gulp-build-branch');
-
-gulp.task('githubPages', function githubPages() {
-  return buildBranch({folder: 'demo'});
-});
